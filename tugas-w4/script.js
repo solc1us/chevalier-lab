5b6c0975c8d66d62e9f4483224ac8ded35ec16e0
solc1us <cyan.magina@gmail.com>
2025-03-12 17:06:56 +0700

    add tugas week 4 local storage

---
+++ tugas-w4/script.js
@@ -0,0 +1,76 @@
+let toDoLists = [
+	{
+		nama: "Belajar HTML dan CSS",
+		deskripsi:
+			"Mempelajari dasar-dasar HTML dan CSS untuk membuat website yang menarik.",
+		deadline: "2024-12-15",
+	},
+];
+
+function loadData() {
+	const data = localStorage.getItem("toDoLists");
+	if (data) {
+		toDoLists = JSON.parse(data);
+	}
+}
+
+function saveData() {
+	localStorage.setItem("toDoLists", JSON.stringify(toDoLists));
+}
+
+let toDoListsElement = document.getElementById("daftar-tugas");
+
+function renderToDoLists() {
+	toDoListsElement.innerHTML = "";
+	toDoLists.forEach((toDoList, index) => {
+		let toDoListElement = `
+			<div class="list-tugas">
+					<div class="atribut-list-tugas">
+						<h2>${toDoList.nama}</h2>
+					</div>
+					<div class="atribut-list-tugas">
+						<p>${toDoList.deskripsi}</p>
+					</div>
+					<div class="atribut-list-tugas">
+						<p>Deadline:&nbsp;</p>
+						<p>${toDoList.deadline}</p>
+					</div>
+					<div class="atribut-list-tugas">
+						<button class="delete" onclick="deleteToDoList(${index})">Hapus</button>
+					</div>
+				</div>
+        `;
+		toDoListsElement.innerHTML += toDoListElement;
+	});
+}
+
+function addToDoList() {
+	const newNama = document.getElementById("nama").value;
+	const newDeskripsi = document.getElementById("deskripsi").value;
+	const newDeadline = document.getElementById("deadline").value;
+
+	if (
+		newNama &&
+		newDeskripsi &&
+		newNama.length > 1 &&
+		newDeskripsi.length > 1
+	) {
+		const newData = {
+			nama: newNama,
+			deskripsi: newDeskripsi,
+			deadline: newDeadline,
+		};
+		toDoLists.push(newData);
+		saveData();
+		renderToDoLists();
+	}
+}
+
+function deleteToDoList(index) {
+	toDoLists.splice(index, 1);
+	saveData();
+	renderToDoLists();
+}
+
+loadData();
+renderToDoLists();
